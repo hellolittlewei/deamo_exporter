87028edab5e46d9334c18d4a54e3461bce7242f4
付先森 <48649837+hellolittlewei@users.noreply.github.com>
2024-05-08 16:54:30 +0800

    Update collector.go
    Remove extra comments

--- collector/collector.go
+++ collector/collector.go
@@ -1,18 +1,4 @@
 // collector.go主要实现对收集器(prometheus.Collector)的组合.
-// 它是非常必要的,如果您要使用它请看下面的离职.
-
-// 先创建您的收集器NewCollector()
-// func NewCollector() prometheus.Collector {
-//		当然您的MyMetrics必须实现Collector接口
-//  	return &MyMetrics{
-//  	my_metrics: prometheus.NewDesc("you_metrics_name", "", []string{}, prometheus.Labels{}),
-//  	}
-//  }
-//	调用生成器函数(MakeSNC)进行注册.
-//	func init()  {
-//		MakeSNC(NewCollector)
-//	}
-
 package collector
 
 import "github.com/prometheus/client_golang/prometheus"
